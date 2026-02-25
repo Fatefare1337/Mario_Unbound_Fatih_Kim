def2f7121f806ff6d0b4eed4089188029229936a
Fatih KASIMLAR <F.Kasimlar@hak-bregenz.at>
2026-02-25 09:23:10 +0100

    bitte geh

--- Form1.cs
+++ Form1.cs
@@ -14,6 +14,7 @@ namespace Mario_Unbound
      */
     public partial class Form1 : Form
     {
+        bool bittegehen = false;
         bool angemeldet = false;
         ComboBox cmb_Avatarbild;
         PictureBox picture;
